68c9b6f58cec60aaeb6085cb02ad0f04f9deae70
Fabrizio Giordanelli <fabrigiorda9999@gmail.com>
2025-07-18 23:53:40 +0000

    npm install firebase
    Luego, inicializa Firebase y comienza a usar los

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,20 +3,15 @@
 import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// ====================================================================================
-// TODO: ¡ACCIÓN REQUERIDA!
-// 1. Ve a tu proyecto en la Consola de Firebase.
-// 2. Ve a Configuración del Proyecto (el engranaje ⚙️).
-// 3. En la sección "Tus apps", copia el objeto `firebaseConfig` de tu aplicación web.
-// 4. Pega ese objeto aquí para reemplazar el que está debajo.
-// ====================================================================================
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "PON_TU_API_KEY_AQUI",
-  authDomain: "PON_TU_AUTH_DOMAIN_AQUI",
-  projectId: "PON_TU_PROJECT_ID_AQUI",
-  storageBucket: "PON_TU_STORAGE_BUCKET_AQUI",
-  messagingSenderId: "PON_TU_MESSAGING_SENDER_ID_AQUI",
-  appId: "PON_TU_APP_ID_AQUI"
+  apiKey: "AIzaSyAYDX_UPo20Ywf77o7jgNEkYXlBBfLRfpU",
+  authDomain: "catalogoxventa.firebaseapp.com",
+  projectId: "catalogoxventa",
+  storageBucket: "catalogoxventa.appspot.com",
+  messagingSenderId: "1001300119916",
+  appId: "1:1001300119916:web:cd69290fab0c6d2a4a6349",
+  measurementId: "G-E71YGEERX3"
 };
 
 // Initialize Firebase
