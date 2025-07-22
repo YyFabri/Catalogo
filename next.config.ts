e4284ab1bd962938bd95b537d8ee19458997c31f
Fabrizio Giordanelli <fabrigiorda9999@gmail.com>
2025-07-22 21:13:26 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.papelerapitti.com.ar',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
