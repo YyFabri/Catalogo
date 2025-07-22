766967ef6b318fd4f5cd2d6d0c37aaefac56f4cb
Fabrizio Giordanelli <fabrigiorda9999@gmail.com>
2025-07-22 21:37:35 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'acdn-us.mitiendanube.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
